ca8d2a4fa6970681f83c0a35b6f3532bbeedad5b
Rohit Gade <rohit14gade@gmail.com>
2025-06-11 11:27:08 +0530

    changes at 6-11-25

--- src/com/array/RevArray.java
+++ src/com/array/RevArray.java
@@ -3,8 +3,8 @@ package com.array;
 public class RevArray {
 	public static void main(String[] args) {
 		int [] arr = {12,23,45,67,18};
-		int  n=arr.length;
-		for(int i=n-1;i>=0;i--) {
+	
+		for(int i=arr.length-1;i>=0;i--) {
 			System.out.println(arr[i]);
 		}
 	}
---
+++ src/com/array/Task.java
@@ -0,0 +1,34 @@
+package com.array;
+
+public class Task {
+
+	
+		
+		public static void main(String[] args) {
+//			Input : n = 2
+//					Output : 9009
+//					9009 is the largest number which is product of two
+//					2-digit numbers. 9009 = 91*99.
+			int n=2;
+			int num=1000;
+			int count=0;
+			for(int i=0;i<=num-1;i++) {
+				if(i%2==0) {
+					count++;
+					
+				}
+				
+			}
+			
+//			Examples
+	//
+//			Input : n = 2
+//			Output : 9009
+//			9009 is the largest number which is product of two
+//			2-digit numbers. 9009 = 91*99.
+	//
+//			Input : n = 3
+//			Output : 906609
+		}
+
+}
